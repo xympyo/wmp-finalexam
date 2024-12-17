ab14561b6831cd6af65f5c50539d18522af2fe3f
Moshe Dayan <xynpyro@gmail.com>
2024-12-18 00:10:52 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/final_exam_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.final_exam_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/shared/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kWhiteColor = const Color(0XFFFFFAF3);
+Color kBlackColor = const Color(0XFF1E1E1E);
+Color kPurpleColor = const Color(0XFF604CC3);
+
+double defaultMargin = 24.0;
+double defaultRadius = 12.0;
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
